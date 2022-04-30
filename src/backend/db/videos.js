1ced2eb6f206d1c6d5f20162acad97084fd46bd8
Shlok Mehra <shlokmehra@rocketmail.com>
2022-04-30 14:57:04 +0530

    Feat : Added Videos Context

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -3,18 +3,41 @@
  * You can add videos of your wish with different attributes
  * */
 
+ import { v4 as uuid } from "uuid";
+
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
+    _id: uuid(),
     title: "Awesome Video about Coding",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
     creator: "Soham Shah",
   },
   {
-    _id: "F_Riqjdh2oM",
+    _id: uuid(),
     title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
     creator: "Sentdex",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "Basics of Jeet Kun Do",
+    creator: "Bruce Lee",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "Naruto Motivational Music",
+    creator: "Mr.Uzumaki",
+    description:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+  },
+  {
+    _id: uuid(),
+    title: "Why Front End will always be in demand",
+    creator: "Shlok",
     description:
       "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
   },
